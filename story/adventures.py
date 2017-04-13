654461d07278bb74b24a5d0b61045f3a2aae025a
lufte <javierayres@gmail.com>
2017-04-13 16:52:15 -0300

    The text if formatted again later in problem_formatted(), so don't format it here.

--- story/adventures.py
+++ story/adventures.py
@@ -40,15 +40,13 @@ class BaseAdventure(object):
 
     @property
     def problem(self):
-        problem = 'Adventure description not found.'
         for file in self.files:
             file = self.get_path(
                 file.format(name='README', language=self.manager.language))
             if os.path.exists(file):
                 with codecs.open(file, encoding='utf-8') as f:
-                    problem = f.read()
-                    break
-        return problem.format(**self.get_context())
+                    return f.read()
+        raise Exception('Adventure description not found')
 
     @property
     def solution(self):
